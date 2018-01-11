0b878b8c7dba2c73e25a43ee1ca5e639be7e1c13
Saurabh Pathak <saurabh.pathak@mastercard.com>
2018-01-11 16:52:28 +0000

    Code for getting client and generating token

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 import { Post } from '../post';
+import * as Simplify from 'simplify-commerce';
+import { WindowRef } from '../window-ref';
 @Component({
   selector: 'app-child-component',
   templateUrl: './child-component.component.html',
-  styleUrls: ['./child-component.component.css']
+  styleUrls: ['./child-component.component.css'],
+  providers: [WindowRef]
 })
 export class ChildComponentComponent implements OnInit {
-
-  constructor(private http: Http) { }
+  SimplifyCommerce: any;
+  constructor(private http: Http, winRef: WindowRef) {
+    this.SimplifyCommerce = winRef.nativeWindow.SimplifyCommerce;
+  }
   title: string;
   toggleChild: boolean;
   appName: string;
@@ -22,6 +27,35 @@ export class ChildComponentComponent implements OnInit {
     this.http.get('https://jsonplaceholder.typicode.com/posts')
     .map(res => res.json())
     .subscribe(posts => this.posts = posts);
+
+    const client = Simplify.getClient({
+      publicKey: 'sbpb_N2UxMjljZDUtN2ZkNi00OGMzLWJmNmMtODRiMzU2MmU1MTM2',
+      privateKey: 'fW0gV8/zgUgkkeURGVFtfTFyuyMYJB8bw/EKbyTGVud5YFFQL0ODSXAOkNtXTToq'
+    });
+    this.SimplifyCommerce.generateToken({
+      key: client.cardtoken.appKeys.publicKey,
+      card: {
+          number: '5555555555554444',
+          cvc: '123',
+          expMonth: '02',
+          expYear: '24'
+      }
+    }, function(data, status) {
+      debugger;
+    });
+    // client.payment.create({
+    //     amount : '1000',
+    //     token : 'f21da65e-f0ab-45cb-b8e6-40b493c3671f',
+    //     description : 'payment description',
+    //     currency : 'USD'
+    // }, function(errData, data){
+    //     if (errData) {
+    //         console.error('Error Message: ' + errData.data.error.message);
+    //         // handle the error
+    //         return;
+    //     }
+    //     console.log('Payment Status: ' + data.paymentStatus);
+    // });
   }
 
   toggleChildComponent() {
