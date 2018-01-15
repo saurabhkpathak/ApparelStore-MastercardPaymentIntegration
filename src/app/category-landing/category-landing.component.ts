4a50580e0ee4ca16a7ad593de2daa6bc7ddd1827
Saurabh Pathak <saurabh.pathak@mastercard.com>
2018-01-15 15:37:22 +0000

    finishing the APIs and consuming them to make payments

--- src/app/category-landing/category-landing.component.ts
+++ src/app/category-landing/category-landing.component.ts
@@ -3,22 +3,57 @@ import { Apparel } from '../apparel';
 import { forEach } from '@angular/router/src/utils/collection';
 import { ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
+import { WindowRef } from '../window-ref';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-category-landing',
   templateUrl: './category-landing.component.html',
-  styleUrls: ['./category-landing.component.css']
+  styleUrls: ['./category-landing.component.css'],
+  providers: [WindowRef]
 })
 export class CategoryLandingComponent implements OnInit {
   apparels: Apparel[];
   routeName: any;
-  constructor(private route: ActivatedRoute, private http: Http) { }
+  SimplifyCommerce: any;
+  constructor(private route: ActivatedRoute, private http: Http,
+    private winRef: WindowRef, private changeRef: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.apparels = [];
+    this.SimplifyCommerce = this.winRef.nativeWindow.SimplifyCommerce;
     this.routeName = this.route.snapshot.paramMap.get('categoryName');
     this.populateApparels();
-    this.http.get('http://localhost:8080/').map(res => res.json()).subscribe(data => this.routeName = data.name);
+    this.generateTokenAndFinishPayment();
+  }
+
+  generateTokenAndFinishPayment() {
+    this.getPublicKey()
+    .subscribe((res) => {
+      this.SimplifyCommerce.generateToken({
+        key: res.publicKey,
+        card: {
+            number: '5555555555554444',
+            cvc: '123',
+            expMonth: '02',
+            expYear: '24'
+        }
+      }, this.finishPayment.bind(this));
+    });
+  }
+
+  getPublicKey() {
+    return this.http.get('http://localhost:8080?type=key')
+      .map(res => res.json());
+  }
+
+  finishPayment(data) {
+    this.http.get('http://localhost:8080?type=payment&id=' + data.id)
+      .map(res => res.json())
+      .subscribe((res) => {
+        this.routeName = res.paymentStatus;
+        this.changeRef.detectChanges();
+      });
   }
 
   populateApparels() {
